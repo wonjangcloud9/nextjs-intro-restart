7eed775e9b19483c19c3ba371888520f952b794f
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-06-06 16:59:17 +0900

    styles js

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,20 +1,27 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
-  console.log(router);
   return (
     <nav>
       <Link href="/">
-        <a className={router.pathname === "/" ? styles.active : ""}>Home</a>
+        <a className={router.pathname === "/" ? "active" : ""}>Home</a>
       </Link>
       <Link href="/about">
-        <a className={router.pathname === "/about" ? styles.active : ""}>
-          About
-        </a>
+        <a className={router.pathname === "/about" ? "active" : ""}>About</a>
       </Link>
+      <style jsx>{`
+        nav {
+          background-color: tomato;
+        }
+        a {
+          text-decoration: none;
+        }
+        .active {
+          color: yellow;
+        }
+      `}</style>
     </nav>
   );
 }
