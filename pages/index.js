cb5fb76429efd004bb97e9096d8d6660e6a20ec8
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-06-06 16:21:57 +0900

    routing

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,10 @@
-import { useState } from "react";
+import NavBar from "../components/NavBar";
 
 export default function Home() {
-  const [counter, setCounter] = useState(0);
   return (
     <div>
-      <h1>Hi {counter}</h1>
-      <button
-        onClick={() => {
-          setCounter((prev) => prev + 1);
-        }}
-      >
-        Click Me
-      </button>
+      <NavBar />
+      <h1>Hello</h1>
     </div>
   );
 }
