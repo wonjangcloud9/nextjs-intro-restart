06718e85250346b2cf3636fe318d1cd166c225cd
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-06-06 16:10:36 +0900

    pre-rendering

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
+
 export default function Home() {
+  const [counter, setCounter] = useState(0);
   return (
     <div>
-      <h1>Hi</h1>
+      <h1>Hi {counter}</h1>
+      <button
+        onClick={() => {
+          setCounter((prev) => prev + 1);
+        }}
+      >
+        Click Me
+      </button>
     </div>
   );
 }
